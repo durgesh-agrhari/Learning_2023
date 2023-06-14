7020ab9c27b83f679b2f87802fb98199b063b118
Durgesh Agrahari <durgesh@pop-os.localdomain>
2023-06-14 14:04:23 +0530

    Add all day-1 program code

--- Module-1/Day-1/1 biggest of two number using if else.c
+++ Module-1/Day-1/1 biggest of two number using if else.c
@@ -1,17 +1,21 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main()
-{
-    int a,b;
-    scanf("%d%d", &a,&b);
-    
-    if(a>b)
-    {
-        printf("A is greater",a);
+int main() {
+    int num1, num2;
+
+    printf("Enter the first number: ");
+    scanf("%d", &num1);
+
+    printf("Enter the second number: ");
+    scanf("%d", &num2);
+
+    if (num1 > num2) {
+        printf("%d is greater than %d\n", num1, num2);
+    } else if (num1 < num2) {
+        printf("%d is greater than %d\n", num2, num1);
+    } else {
+        printf("Both numbers are equal\n");
     }
-    else
-    {
-        printf("B is greater",b);
-    }
+
     return 0;
 }
