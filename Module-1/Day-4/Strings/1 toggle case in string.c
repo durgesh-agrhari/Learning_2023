41190fb8aee857732453c8d57dc57c4621b8432e
durgesh-agrhari <durgeshita@gmail.com>
2023-06-20 16:43:37 +0530

    Add Day-4 String one answer in c

---
+++ Module-1/Day-4/Strings/1 toggle case in string.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+ 
+int main()
+{
+    char s[1000];  
+    int i;
+ 
+    printf("Enter  the string : ");
+    gets(s);
+    
+ 
+     
+    for(i=0;s[i];i++)  
+    {
+        if(s[i]>=65 && s[i]<=90)
+         s[i]+=32;
+        else if(s[i]>=97 && s[i]<=122)
+         s[i]-=32;
+ 	}
+ 	
+     
+    printf("string in togglecase ='%s'\n",s);
+
+
+    return 0;
+}
