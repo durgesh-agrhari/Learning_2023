838248c82ab95406fdb56151379de3d9cac1493e
durgesh-agrhari <durgeshita@gmail.com>
2023-06-21 10:50:04 +0530

    Add Day-6 L1 two answers in c

---
+++ Module-1/Day-6/L1 Activities ( Array of Structures )/2 Write a function to initialize all members in the above array of structures.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct Student {
+    int rollno;
+    char name[20];
+    float marks;
+};
+
+void parseInputString(const char* inputString, struct Student* students, int numStudents) {
+    const char* delimiter = " ";
+    char* token = strtok((char*)inputString, delimiter);
+
+    for (int i = 0; i < numStudents; i++) {
+        if (token == NULL) {
+            printf("Error: Insufficient input!\n");
+            return;
+        }
+
+        students[i].rollno = atoi(token);
+
+        token = strtok(NULL, delimiter);
+
+        if (token == NULL) {
+            printf("Error: Insufficient input!\n");
+            return;
+        }
+
+        strncpy(students[i].name, token, sizeof(students[i].name) - 1);
+        students[i].name[sizeof(students[i].name) - 1] = '\0';
+
+        token = strtok(NULL, delimiter);
+
+        if (token == NULL) {
+            printf("Error: Insufficient input!\n");
+            return;
+        }
+
+        students[i].marks = atof(token);
+
+        token = strtok(NULL, delimiter);
+    }
+}
+
+void initializeStudents(struct Student* students, int numStudents) {
+    for (int i = 0; i < numStudents; i++) {
+        students[i].rollno = 0;
+        strcpy(students[i].name, "");
+        students[i].marks = 0.0;
+    }
+}
+
+int main() {
+    int numStudents;
+    printf("Enter the number of students: ");
+    scanf("%d", &numStudents);
+    getchar();  // Clearing the newline character from the input buffer
+
+    struct Student* students = (struct Student*)malloc(numStudents * sizeof(struct Student));
+
+    printf("Enter the student details (rollno name marks) separated by spaces:\n");
+
+    for (int i = 0; i < numStudents; i++) {
+        char inputString[100];
+        fgets(inputString, sizeof(inputString), stdin);
+        inputString[strcspn(inputString, "\n")] = '\0';  // Removing the trailing newline character
+
+        parseInputString(inputString, &students[i], 1);
+    }
+
+    // Print the initialized array of structures
+    printf("\nStudent Details:\n");
+    for (int i = 0; i < numStudents; i++) {
+        printf("Roll No: %d, Name: %s, Marks: %.2f\n", students[i].rollno, students[i].name, students[i].marks);
+    }
+
+    free(students);  // Freeing the allocated memory
+
+    return 0;
+}
